9175e4c05de9a37e30cae0b7c3d6a6ae73c3bcc1
varunamachi <varunamachi@gmail.com>
2017-01-27 18:47:56 +0530

    Implemented DB initialization and basic commandline

--- data/postgres_init.go
+++ data/postgres_init.go
@@ -4,50 +4,67 @@ import (
 	"fmt"
 	"log"
 
+	"database/sql"
+
 	"github.com/jmoiron/sqlx"
 )
 
-const exists = `SELECT COUNT (*) FROM sqlite_master 
-        WHERE type = 'table' AND name = user;`
+const exists = `SELECT EXISTS (
+	SELECT table_name FROM information_schema.tables WHERE 
+	table_schema='public' AND table_name='%s');`
 
-var queries = [...]string{
+//CreateQuery - Represents a query that creates a table
+type CreateQuery struct {
+	TableName   string
+	QueryString string
+}
 
-	`CREATE TABLE user(
-		user_name     VARCHAR( 255 ) NOT NULL,
-		first_name    VARCHAR( 255 ),
-		second_name   VARCHAR( 255 ),
-		email         VARCHAR( 255 ) NOT NULL,
-		PRIMARY KEY( user_name ),
-		UNIQUE(email)
-    );`,
-
-	`CREATE TABLE user_password(
-    	user_name   VARCHAR( 255 ) NOT NULL,
-    	hash        VARCHAR( 255 ) NOT NULL,
-    	PRIMARY KEY( user_name ),
-    	FOREIGN KEY( user_name ) REFERENCES user( user_name ) 
-			ON DELETE CASCADE,
-    );`,
-
-	`CREATE TABLE group(
-		group_id	VARCHAR( 256 ) NOT NULL
-    	name        VARCHAR( 256 ) NOT NULL,
-    	owner       VARCHAR( 256 ) NOT NULL,
-    	description TEXT NOT NULL,
-        visiblity   VARCHAR( 256 ) NOT NULL
-    	PRIMARY KEY( group_id ),
-    	FOREIGN KEY( owner ) REFERENCES user( user_name )
-    );`,
-
-	`CREATE TABLE user_to_group(
-    	group_id    VARCHAR( 256 ) NOT NULL,
-    	user_name   VARCHAR( 256 ) NOT NULL,
-    	FOREIGN KEY( group_id ) REFERENCES user_group( group_id )
-			ON DELETE CASCADE,
-    	FOREIGN KEY( user_name ) REFERENCES user( user_name ),
-    	PRIMARY KEY( group_id, user_name )
-			ON DELETE CASCADE
-    );`,
+var queries = [...]CreateQuery{
+	CreateQuery{
+		TableName: "fc_user",
+		QueryString: `CREATE TABLE fc_user(
+			user_name     VARCHAR( 255 ) NOT NULL,
+			first_name    VARCHAR( 255 ),
+			second_name   VARCHAR( 255 ),
+			email         VARCHAR( 255 ) NOT NULL,
+			PRIMARY KEY( user_name ),
+			UNIQUE(email)
+    	);`,
+	},
+	CreateQuery{
+		TableName: "fc_user_password",
+		QueryString: `CREATE TABLE fc_user_password(
+    		user_name   VARCHAR( 255 ) NOT NULL,
+    		hash        VARCHAR( 255 ) NOT NULL,
+    		PRIMARY KEY( user_name ),
+    		FOREIGN KEY( user_name ) REFERENCES fc_user( user_name ) 
+				ON DELETE CASCADE
+    	);`,
+	},
+	CreateQuery{
+		TableName: "fc_group",
+		QueryString: `CREATE TABLE fc_group(
+			group_id	VARCHAR( 256 ) NOT NULL,
+    		name        VARCHAR( 256 ) NOT NULL,
+    		owner       VARCHAR( 256 ) NOT NULL,
+    		description TEXT NOT NULL,
+    	    visiblity   VARCHAR( 256 ) NOT NULL,
+    		PRIMARY KEY( group_id ),
+    		FOREIGN KEY( owner ) REFERENCES fc_user( user_name )
+    	);`,
+	},
+	CreateQuery{
+		TableName: "fc_user_to_group",
+		QueryString: `CREATE TABLE fc_user_to_group(
+    		group_id    VARCHAR( 256 ) NOT NULL,
+    		user_name   VARCHAR( 256 ) NOT NULL,
+    		PRIMARY KEY( group_id, user_name ),
+    		FOREIGN KEY( group_id ) REFERENCES fc_group( group_id ) 
+				ON DELETE CASCADE,
+    		FOREIGN KEY( user_name ) REFERENCES fc_user( user_name ) 
+				ON DELETE CASCADE
+    	);`,
+	},
 }
 
 //PostgresStore - datasource implemented using postgress
@@ -55,57 +72,54 @@ type PostgresStore struct {
 	*sqlx.DB
 }
 
-//Options - options for connecting to sqlite database
-type Options struct {
+//PsqlOptions - options for connecting to sqlite database
+type PsqlOptions struct {
 	UserName string
 	Password string
+	Host     string
+	Port     int
 	DBName   string
 }
 
-//Init - initializes the datastore
-func Init(options *Options) (*PostgresStore, error) {
-	dbArgs := fmt.Sprintf(
-		"user=%s password=%s dbname=%s sslmode=disable",
+//PsqlInit - initializes the datastore
+func PsqlInit(options *PsqlOptions) (pgdb *PostgresStore, err error) {
+	//postgres://<user>:<password>@<host>:<port>/<dbname>?<params>
+	connInfo := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
 		options.UserName,
 		options.Password,
+		options.Host,
+		options.Port,
 		options.DBName)
-	db, err := sqlx.Connect("postgres", dbArgs)
+	var db *sqlx.DB
+	db, err = sqlx.Connect("postgres", connInfo)
 	if err != nil {
 		log.Print(err)
 		return nil, err
 	}
 	defer db.Close()
-	var pgdb *PostgresStore
+
 	if err = db.Ping(); err == nil {
-		row := db.QueryRow(exists)
-		var count int
-		err = row.Scan(&count)
-		if err == nil {
-			if count == 0 {
-				pgdb, err = create(options, db)
-			} else {
-				pgdb, err = connect(options)
-			}
-		} else {
-			log.Print(err)
-			return nil, err
-		}
+		pgdb, err = create(options, db)
+	}
+	if err != nil {
+		log.Println(err)
 	}
 	return pgdb, err
 }
 
 //connect - connects to a sqlite database file
-func connect(options *Options) (*PostgresStore, error) {
-	dbArgs := fmt.Sprintf(
-		"user=%s password=%s dbname=%s sslmode=disable",
+func connect(options *PsqlOptions) (*PostgresStore, error) {
+	connInfo := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
 		options.UserName,
 		options.Password,
+		options.Host,
+		options.Port,
 		options.DBName)
-	mdb, err := sqlx.Connect("postgres", dbArgs)
+	mdb, err := sqlx.Connect("postgres", connInfo)
 	if err != nil {
 		log.Print(err)
 	} else if err = mdb.Ping(); err != nil {
-		log.Printf("Could not connect to mysql database: %s", err)
+		log.Printf("Could not connect to postgress database: %s", err)
 	} else {
 		log.Print("Database opened successfuly")
 	}
@@ -113,17 +127,40 @@ func connect(options *Options) (*PostgresStore, error) {
 }
 
 //create - connects to a sqlite database file and creates schema
-func create(options *Options, db *sqlx.DB) (*PostgresStore, error) {
+func create(options *PsqlOptions, db *sqlx.DB) (*PostgresStore, error) {
 	mdb, err := connect(options)
 	if err == nil {
-		for index, query := range queries {
-			_, err = mdb.Exec(query)
-			if err != nil {
-				log.Printf(`Failed to create database, query %d failed: %s`,
-					index, err)
-				break
+		for _, query := range queries {
+			if !tableExists(db, query.TableName) {
+				_, err = mdb.Exec(query.QueryString)
+				if err != nil {
+					log.Printf(`Could not create table %s: %s`,
+						query.TableName, err)
+					// break
+				} else {
+					log.Printf("Created table %s", query.TableName)
+				}
+			} else {
+				log.Printf("Table %s already exists, nothing to do!",
+					query.TableName)
 			}
 		}
 	}
 	return mdb, err
+}
+
+func tableExists(db *sqlx.DB, tableName string) (has bool) {
+	query := fmt.Sprintf(exists, tableName)
+	rows := db.QueryRowx(query)
+	err := rows.Err()
+	if err == nil {
+		err = rows.Scan(&has)
+	}
+	if err == sql.ErrNoRows {
+		has = false
+	} else if err != nil {
+		log.Fatalf("Could not initialize database: %s", err)
+		has = false
+	}
+	return has
 }
--- main.go
+++ main.go
@@ -1,13 +1,54 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"syscall"
 
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/ssh/terminal"
 
-	_ "github.com/RedchilliSauce/flixcult/data"
+	"github.com/RedchilliSauce/flixcult/data"
 )
 
 func main() {
-	fmt.Println("Hello Cult!")
+
+	var userName string
+	var host string
+	var port int
+	var dbName string
+	var password string
+
+	flag.StringVar(&userName, "user-name", "", "Database user name")
+	flag.StringVar(&host, "host", "localhost", "Database host")
+	flag.StringVar(&dbName, "db-name", "flixcult", "Name of the database")
+	flag.IntVar(&port, "port", 5432, "Database port")
+	flag.StringVar(&password, "password", "",
+		"Password, will be asked later if not provided")
+	flag.Parse()
+	if len(password) == 0 && len(userName) != 0 {
+		fmt.Printf("Password for %s: ", userName)
+		pbyte, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			fmt.Print(err)
+		} else {
+			password = string(pbyte)
+		}
+	} else if len(userName) == 0 {
+		log.Fatal("Insufficient parameters for FlixCult to run!")
+	}
+	options := &data.PsqlOptions{
+		UserName: userName,
+		Password: password,
+		Host:     host,
+		Port:     port,
+		DBName:   dbName}
+	// fmt.Println(options)
+	db, err := data.PsqlInit(options)
+	if err == nil {
+		data.SetStore(db)
+	} else {
+		log.Fatal("Failed to initialize database")
+	}
 }
